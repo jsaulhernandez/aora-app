a787463db445eb8c4d1f50173a73f10582f92361
Sony <jsaulhernandez98@gmail.com>
2024-12-04 14:06:17 -0600

    tag: adding custom input component

--- data/types/index.ts
+++ data/types/index.ts
@@ -1,7 +1,19 @@
+import { KeyboardTypeOptions } from "react-native";
+
 export type CustomButtonProps = {
   title: string;
   handlePress: () => void;
   containerStyles?: string;
   textStyles?: string;
   isLoading?: boolean;
+};
+
+export type FormFieldProps = {
+  title: string;
+  value: string;
+  placeholder?: string;
+  handleChangeText: (e: string) => void;
+  otherStyles?: string;
+  keyboardType?: KeyboardTypeOptions;
+  typeInput?: "text" | "password";
 };
