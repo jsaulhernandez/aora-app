6b68cdc0b5267d4c34b6cc656a89d38fe21b7016
Sony <jsaulhernandez98@gmail.com>
2024-12-04 13:28:53 -0600

    first commit

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,121 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+
+import { icons } from "../../constants";
+
+const TabIcon = ({
+  icon,
+  color,
+  name,
+  focused,
+}: {
+  icon: any;
+  color: string;
+  name: string;
+  focused: boolean;
+}) => {
+  return (
+    <View className="flex items-center justify-center gap-2">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+
+      <Text
+        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+        style={{ color: color }}
+      >
+        {name}
+      </Text>
+    </View>
+  );
+};
+
+const TabsLayout = () => {
+  return (
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: "#FFA001",
+          tabBarInactiveTintColor: "#CDCDE0",
+          tabBarStyle: {
+            backgroundColor: "#161622",
+            borderTopWidth: 1,
+            borderTopColor: "#232533",
+            height: 84,
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                color={color}
+                icon={icons.home}
+                focused={focused}
+                name="Home"
+              />
+            ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="bookmark"
+          options={{
+            title: "Bookmark",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                color={color}
+                icon={icons.bookmark}
+                focused={focused}
+                name="Bookmark"
+              />
+            ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="create"
+          options={{
+            title: "Create",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                color={color}
+                icon={icons.plus}
+                focused={focused}
+                name="Create"
+              />
+            ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                color={color}
+                icon={icons.profile}
+                focused={focused}
+                name="Profile"
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
+  );
+};
+
+export default TabsLayout;
---
+++ app/search/[query].tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { useLocalSearchParams } from "expo-router";
+
+const Search = () => {
+  const { query } = useLocalSearchParams();
+  return (
+    <View>
+      <Text>Search</Text>
+    </View>
+  );
+};
+
+export default Search;
