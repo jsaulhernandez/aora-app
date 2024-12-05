27b3975a2da2a0b85e7c67a86dd23ebb0a9955bc
Sony <jsaulhernandez98@gmail.com>
2024-12-05 11:07:27 -0600

    tag: finished UI to profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,87 @@
-import { View, Text } from "react-native";
+import { router } from "expo-router";
+import { View, FlatList, TouchableOpacity, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+// components
+import EmptyState from "@/components/EmptyState";
+import InfoBox from "@/components/InfoBox";
+import VideoCard from "@/components/VideoCard";
+// hooks
+import useAppwrite from "@/hooks/useAppwrite";
+// contexts
+import { useGlobalContext } from "@/context/GlobalProvider";
+// constants
+import { icons } from "@/constants";
+// lib
+import { getUserPosts, signOut } from "@/lib/appwrite";
 
 const Profile = () => {
+  const { user, setUser, setIsLogged } = useGlobalContext();
+  const { data: posts } = useAppwrite(() => getUserPosts(user?.$id ?? ""));
+
+  const logout = async () => {
+    await signOut();
+    setUser(null);
+    setIsLogged(false);
+
+    router.replace("/sign-in");
+  };
+
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <VideoCard post={item} key={item.$id} />}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="No videos found for this profile"
+          />
+        )}
+        ListHeaderComponent={() => (
+          <View className="w-full flex justify-center items-center mt-6 mb-12 px-4">
+            <TouchableOpacity
+              onPress={logout}
+              className="flex w-full items-end mb-10"
+            >
+              <Image
+                source={icons.logout}
+                resizeMode="contain"
+                className="w-6 h-6"
+              />
+            </TouchableOpacity>
+
+            <View className="w-16 h-16 border border-secondary rounded-lg flex justify-center items-center">
+              <Image
+                source={{ uri: user?.avatar }}
+                className="w-[90%] h-[90%] rounded-lg"
+                resizeMode="cover"
+              />
+            </View>
+
+            <InfoBox
+              title={user?.username ?? ""}
+              containerStyles="mt-5"
+              titleStyles="text-lg"
+            />
+
+            <View className="mt-5 flex flex-row">
+              <InfoBox
+                title={`${posts.length || 0}`}
+                subtitle="Posts"
+                titleStyles="text-xl"
+                containerStyles="mr-10"
+              />
+              <InfoBox
+                title="1.2k"
+                subtitle="Followers"
+                titleStyles="text-xl"
+              />
+            </View>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 };
 
