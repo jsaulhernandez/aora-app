b2b53b2bf9db6705cf77a5b0fc1b31cefa0a89e6
Sony <jsaulhernandez98@gmail.com>
2024-12-05 11:05:57 -0600

    tag: updating search input component

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -4,9 +4,9 @@ import { View, TouchableOpacity, Image, TextInput, Alert } from "react-native";
 // constants
 import { icons } from "../constants";
 
-const SearchInput = ({ initialQuery }: { initialQuery?: string }) => {
+const SearchInput = ({ initialQuery = "" }: { initialQuery: string }) => {
   const pathname = usePathname();
-  const [query, setQuery] = useState(initialQuery ?? "");
+  const [query, setQuery] = useState(initialQuery);
 
   return (
     <View className="flex flex-row items-center space-x-4 w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary">
